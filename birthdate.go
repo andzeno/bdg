311326aa0af4f6b6e661a542fdb47e7e06d27bab
Zeno Ando <boku@andzeno.com>
2023-12-31 19:15:43 +1100

    Expand to Random and Specific age generation

--- birthdate.go
+++ birthdate.go
@@ -5,12 +5,9 @@ import (
 	"time"
 )
 
-// randBirthAge generates a random age from 18 to 88 and the birth date from time.Now()
-func RandBirthAge() (int, string) {
+// genBirthAge generates the age
+func genBirthAge(age int) (int, string) {
 	now := time.Now()
-
-	// Generate a random number between 18 and 88
-	age := rand.Intn(71) + 18
 
 	// Compute and generate the date for the random age
 	birthDate := now.AddDate(-age, 0, 0).Format("2006-01-02")
@@ -18,3 +15,19 @@ func RandBirthAge() (int, string) {
 	return age, birthDate
 }
 
+// RandBirthAge generates a random age from 18 to 88 and the birth date from time.Now()
+func RandBirthAge() (int, string) {
+	// Generate a random number between 18 and 88
+	randAge := rand.Intn(71) + 18
+
+	age, birthDate := genBirthAge(randAge)
+
+	return age, birthDate
+}
+
+// SpecBirthAge generates the birthdate for the supplied age target
+func SpecBirthAge(age int) string {
+	_, birthDate := genBirthAge(age)
+
+	return birthDate
+}
