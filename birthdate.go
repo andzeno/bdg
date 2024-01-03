b85db69f50931f74a067a59b4e0bbfb89aae0ce0
Zeno Ando <boku@andzeno.com>
2024-01-03 21:51:06 +1100

    Move towards struct type

--- birthdate.go
+++ birthdate.go
@@ -5,29 +5,44 @@ import (
 	"time"
 )
 
-// genBirthAge generates the age
-func genBirthAge(age int) (int, string) {
+type BirthDate struct {
+	Years  int
+	Months int
+	Days   int
+	Date   string
+}
+
+// Generate outputs a BirthDate type
+func (b *BirthDate) Generate(values ...int) {
 	now := time.Now()
+	// Generate between 18 and 88 for years
+	randYears := rand.Intn(71) + 18
+	// 0-11 for month
+	randMonths := rand.Intn(5) + rand.Intn(6)
+	// 0-30 for days
+	randDays := rand.Intn(15) + rand.Intn(15)
+
+	// Generate birthdate data based on parameter count
+	paramsCnt := len(values)
+	if paramsCnt == 1 {
+		b.Years = values[0]
+		b.Months = randMonths
+		b.Days = randDays
+	} else if paramsCnt == 2 {
+		b.Years = values[0]
+		b.Months = values[1]
+		b.Days = randDays
+	} else if paramsCnt >= 3 {
+		b.Years = values[0]
+		b.Months = values[1]
+		b.Days = values[2]
+	} else if paramsCnt == 0 {
+		b.Years = randYears
+		b.Months = randMonths
+		b.Days = randDays
+	}
 
 	// Compute and generate the date for the random age
-	birthDate := now.AddDate(-age, 0, 0).Format("2006-01-02")
+	b.Date = now.AddDate(-b.Years, b.Months, b.Days).Format("2006-01-02")
 
-	return age, birthDate
-}
-
-// RandBirthAge generates a random age from 18 to 88 and the birth date from time.Now()
-func RandBirthAge() (int, string) {
-	// Generate a random number between 18 and 88
-	randAge := rand.Intn(71) + 18
-
-	age, birthDate := genBirthAge(randAge)
-
-	return age, birthDate
-}
-
-// SpecBirthAge generates the birthdate for the supplied age target
-func SpecBirthAge(age int) string {
-	_, birthDate := genBirthAge(age)
-
-	return birthDate
 }
--- birthdate_test.go
+++ birthdate_test.go
@@ -1,24 +1,58 @@
 package bdg
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestRandBirthAge(t *testing.T) {
+func TestGenerate(t *testing.T) {
 	// Call function to test
-	age, birthDate := RandBirthAge()
+	b := BirthDate{}
+	b.Generate()
 
-	// Check if age is between 18 and 88
-	if age < 18 || age > 88 {
-		t.Errorf("Expected generated age to be between 18-88, got %d", age)
+	// Check if years is between 18 and 88
+	if b.Years < 18 || b.Years > 88 {
+		t.Errorf("Expected generated years to be between 18-88, got %d", b.Years)
+	}
+
+	// Check if months is beyond 11
+	if b.Months > 11 {
+		t.Errorf("Expected generated months doesn't exceed 11, got %d", b.Months)
+	}
+
+	// Check if days is beyond 30
+	if b.Days > 30 {
+		t.Errorf("Expected generated days doesn't exceed 30, got %d", b.Days)
+	}
+
+	fmt.Println(b.Date)
+
+	// Check if date generated is valid
+	_, err := time.Parse("2006-01-02", b.Date)
+
+	b.Generate(32)
+	// Check if years is between 18 and 88
+	if b.Years != 32 {
+		t.Errorf("Expected generated years to be 32, got %d", b.Years)
+	}
+
+	// Check if months is beyond 11
+	if b.Months > 11 {
+		t.Errorf("Expected generated months doesn't exceed 11, got %d", b.Months)
+	}
+
+	// Check if days is beyond 30
+	if b.Days > 30 {
+		t.Errorf("Expected generated days doesn't exceed 30, got %d", b.Days)
 	}
 
 	// Check if date generated is valid
-	_, err := time.Parse("2006-01-02", birthDate)
+	_, err = time.Parse("2006-01-02", b.Date)
 
 	if err != nil {
-		t.Errorf("Expected generated birth date %s isn't a valid date: %v", birthDate, err)
+		t.Errorf("Expected generated birth date %s isn't a valid date: %v", b.Date, err)
 	}
 
+	fmt.Println(b.Date)
 }
